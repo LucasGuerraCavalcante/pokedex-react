3cec2ab25b50ed86c85d180ae75b40ae8dc08927
Lucas Guerra <lucasguerratee@gmail.com>
2020-06-26 01:01:16 -0300

    backend routes

---
+++ backend/src/routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+
+import pokeAPIController from './controllers/pokeAPIController'
+
+const routes = Router()
+
+routes.get('/generation/:id/species', pokeAPIController.species )
+routes.get('/pokemon/:id', pokeAPIController.details )
+
+export default routes;
