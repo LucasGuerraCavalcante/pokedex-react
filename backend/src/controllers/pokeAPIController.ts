63e84534253fefd9dd3b95fd3fc6457ef83ca43b
Lucas Guerra <lucasguerratee@gmail.com>
2020-06-26 16:17:30 -0300

    formating the raw data to show just what I want

--- backend/src/controllers/pokeAPIController.ts
+++ backend/src/controllers/pokeAPIController.ts
@@ -11,8 +11,14 @@ export default {
         fetch(url, { method: 'get' })
             .then(res => res.json())
             .then(data => {
-                const pokemons = data 
-                return response.json(pokemons)
+                const generation = {
+                    id: data.id,
+                    name: data.name,
+                    mainRegion: data.main_region.name,
+                    pokemons: data.pokemon_species
+                } 
+
+                return response.json(generation)
             })
             .catch(err => console.error(err))
     },
@@ -25,11 +31,26 @@ export default {
         fetch(url, { method: 'get' })
             .then(res => res.json())
             .then(data => {
-                const pokemon = data 
+
+                const pokemon = {
+                    id: data.id,
+                    name: data.name,
+                    height: data.height,
+                    weight: data.weight,
+                    types: data.types,
+                    stats: data.stats,
+                    sprites: {
+                        male: data.sprites.front_default,
+                        female: data.sprites.front_female,
+                        shinyMale: data.sprites.front_shiny,
+                        shinyFemale: data.sprites.front_shiny_female,
+                    },
+                    abilities: data.abilities
+                }
+
                 return response.json(pokemon)
             })
             .catch(err => console.error(err))
-
     }
 
 } 
