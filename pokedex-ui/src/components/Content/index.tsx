a16fa7e34ef1473fc85443e5501224894f98e307
Lucas Guerra <lucasguerratee@gmail.com>
2020-06-27 01:36:16 -0300

    content component

---
+++ pokedex-ui/src/components/Content/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const Content: React.FC = () => {
+  return (
+    <div className="content">
+        
+    </div>
+  );
+};
+
+export default Content;
