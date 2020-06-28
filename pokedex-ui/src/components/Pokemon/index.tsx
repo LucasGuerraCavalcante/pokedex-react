99ba4a5e4f41817e1081ae453ac0aa812f343e51
Lucas Guerra <lucasguerratee@gmail.com>
2020-06-27 22:31:08 -0300

    pokemon content

---
+++ pokedex-ui/src/components/Pokemon/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+
+const Pokemon: React.FC = () => {
+  return (
+    <div className="pokemon">
+        <span>A</span>
+        <h3>Pokemon</h3>
+        <span>A</span>     
+    </div>
+  );
+};
+
+export default Pokemon;
