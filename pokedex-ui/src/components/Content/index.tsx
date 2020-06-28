d11aba0d2879103173bbe8723c20cd0599eeb670
Lucas Guerra <lucasguerratee@gmail.com>
2020-06-27 22:29:58 -0300

    content container component

--- pokedex-ui/src/components/Content/index.tsx
+++ pokedex-ui/src/components/Content/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 import './styles.css';
 
+import Header from '../Header';
+import Form from '../Form';
+import Pokemon from '../Pokemon';
+
 const Content: React.FC = () => {
   return (
-    <div className="content">
-        
+    <div className="container">
+        <Header />
+        <Form />
+        <Pokemon />
     </div>
   );
 };
