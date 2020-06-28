45742bc27acb51edced6f9903985f04bac1cd9f6
Lucas Guerra <lucasguerratee@gmail.com>
2020-06-27 22:30:09 -0300

    form component

---
+++ pokedex-ui/src/components/Form/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.css';
+
+const Form: React.FC = () => {
+  return (
+    <div className="form">
+        <span>A</span>
+        <h3>Form</h3>
+        <span>A</span>     
+    </div>
+  );
+};
+
+export default Form;
