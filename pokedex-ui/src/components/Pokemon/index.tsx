6727a1e86c4fe20450f6a6ce90c37e269d1e1850
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-07-01 15:33:00 -0300

    getting data from the parent component

--- pokedex-ui/src/components/Pokemon/index.tsx
+++ pokedex-ui/src/components/Pokemon/index.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 import './styles.css'
 
-const Pokemon: React.FC = () => {
+interface pokemon {
+  selectedPokemonName: string;
+}
+
+const Pokemon: React.FC<pokemon> = ({ selectedPokemonName }) => {
 
   return (
     <div className="pokemon">
         <span>A</span>
-        <h3>Pokemon</h3>
+        <h3>{ selectedPokemonName }</h3>
         <span>A</span>     
     </div>
   )
