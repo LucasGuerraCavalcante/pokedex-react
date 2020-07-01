bdc6fa27729e3a01c0266dcfa3269fe0e04ca62c
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-07-01 02:51:20 -0300

    pokemon now is a child component

--- pokedex-ui/src/components/Content/index.tsx
+++ pokedex-ui/src/components/Content/index.tsx
@@ -4,14 +4,12 @@ import './styles.css';
 
 import Header from '../Header';
 import Form from '../Form';
-import Pokemon from '../Pokemon';
 
 const Content: React.FC = () => {
   return (
     <div className="container">
         <Header />
         <Form />
-        <Pokemon />
     </div>
   );
 };
