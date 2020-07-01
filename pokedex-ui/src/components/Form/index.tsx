34fd09ea9c5ceca055a9de903a493e36e9691330
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-07-01 02:51:42 -0300

    form sending the pokemon name

--- pokedex-ui/src/components/Form/index.tsx
+++ pokedex-ui/src/components/Form/index.tsx
@@ -1,14 +1,18 @@
-import React, { useState, useEffect, ChangeEvent } from 'react';
+import React, { useState, useEffect, ChangeEvent } from 'react'
 import axios from 'axios'
 
-import './styles.css';
+import Pokemon from '../Pokemon'
+
+import './styles.css'
 
 const Form: React.FC = () => {
 
   const [pokemons, setPokemons] = useState<string[]>([])
 
   const [selectedGeneration, setSelectedGeneration] = useState<string>('1')
-  const [selectedPokemon, setSelectedPokemon] = useState<string>('1')
+  const [selectedPokemon, setSelectedPokemon] = useState<string>('')
+
+  const [formData, setFormData] = useState({})
 
   useEffect(() => {
       axios.get<string[]>(`http://localhost:3333/generation/${selectedGeneration}/species`)
@@ -25,37 +29,49 @@ const Form: React.FC = () => {
     setSelectedGeneration(generation)
   }
 
-  function findPokemon() {
+  function handleSelectPokemon(event: ChangeEvent<HTMLSelectElement>) {
+    const pokemon = event.target.value
+    setSelectedPokemon(pokemon)
+  }
 
+  function sendPokemon() {
+    const confirmedPokemon = formData
+    setFormData(confirmedPokemon)
   }
 
   return (
-    <form className="form" onSubmit={findPokemon} >
+    <>
+      <form className="form" onSubmit={ sendPokemon } >
 
-        <select id="generationSelect" name="generation" value={ selectedGeneration }
-          onChange={ handleSelectedGeneration } >
-          <option key={1} value="1">Generation 1</option>
-          <option key={2} value="2">Generation 2</option>
-          <option key={3} value="3">Generation 3</option>
-          <option key={4} value="4">Generation 4</option>
-          <option key={5} value="5">Generation 5</option>
-          <option key={6} value="6">Generation 6</option>
-          <option key={7} value="7">Generation 7</option>
-        </select>
+          <select id="generationSelect" name="generation" 
+            value={ selectedGeneration }
+            onChange={ handleSelectedGeneration } >
+            <option key={1} value="1">Generation 1</option>
+            <option key={2} value="2">Generation 2</option>
+            <option key={3} value="3">Generation 3</option>
+            <option key={4} value="4">Generation 4</option>
+            <option key={5} value="5">Generation 5</option>
+            <option key={6} value="6">Generation 6</option>
+            <option key={7} value="7">Generation 7</option>
+          </select>
 
-        <input type="text" list="pokemons" id="pokemonInput" 
-              placeholder="Pokemon's Name" ></input>
-        <datalist id="pokemons">
-          <option value="test">test</option>
+          <input type="text" list="pokemons" id="pokemonInput"
+                placeholder="Pokemon's Name" 
+                value={ selectedPokemon }
+                onChange={() => handleSelectPokemon }></input>
+          <datalist id="pokemons">
 
-          { pokemons.map(pokemon => (
-              <option key={pokemon} value={pokemon}>{pokemon}</option>
-          ))}
+            { pokemons.map(pokemon => (
+                <option key={pokemon} value={pokemon}>{pokemon}</option>
+            ))}
 
-        </datalist>
+          </datalist>
 
-        <button type="submit">Search</button>   
-    </form>
+          <button type="submit">Search</button>   
+      </form>
+
+      <Pokemon />
+    </>
   );
 };
 
