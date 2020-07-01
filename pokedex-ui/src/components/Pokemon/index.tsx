3acb086f12673aa078165906b2caaf79a24e2374
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-07-01 17:19:34 -0300

    pokemon component accessing the backend

--- pokedex-ui/src/components/Pokemon/index.tsx
+++ pokedex-ui/src/components/Pokemon/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import './styles.css'
 
@@ -8,10 +9,23 @@ interface pokemon {
 
 const Pokemon: React.FC<pokemon> = ({ selectedPokemonName }) => {
 
+  const [selectedPokemonData, setSelectedPokemonData] = useState<any>({})
+
+  useEffect(() => {
+    axios.get<any>(`http://localhost:3333/pokemon/${selectedPokemonName}`)
+        .then(response => {
+            const pokemonInfo = response.data
+            setSelectedPokemonData(pokemonInfo)
+        })
+        .catch(err => console.error(err))
+  })
+
   return (
     <div className="pokemon">
-        <span>A</span>
-        <h3>{ selectedPokemonName }</h3>
+        <span >A</span>
+        <h3 >{ selectedPokemonData.name }</h3>
+        <h3 >{ selectedPokemonData.height }</h3>
+        <h3 >{ selectedPokemonData.weight }</h3>
         <span>A</span>     
     </div>
   )
