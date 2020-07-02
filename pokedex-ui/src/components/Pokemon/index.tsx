7ad341845b78cf903e064988d6a29014c5de7a07
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-07-02 16:58:14 -0300

    pokemon component showing all necessary data

--- pokedex-ui/src/components/Pokemon/index.tsx
+++ pokedex-ui/src/components/Pokemon/index.tsx
@@ -3,15 +3,47 @@ import axios from 'axios'
 
 import './styles.css'
 
-interface pokemon {
-  selectedPokemonName: string;
+interface pokemonName {
+  selectedPokemonName: string
 }
 
-const Pokemon: React.FC<pokemon> = ({ selectedPokemonName }) => {
+interface pokemonData {
+  id: number,
+  name: string,
+  height: number,
+  weight: number,
+  male: string,
+  female?: string,
+  shinyMale: string,
+  shinyFemale?: string,
+  abilities: string[],
+  types: string[]
+}
+
+const Pokemon: React.FC<pokemonName> = ({ selectedPokemonName }) => {
 
   const [pokemonName, setPokemonName] = useState<string>(selectedPokemonName)
 
-  const [selectedPokemonData, setSelectedPokemonData] = useState<any>({})
+  const [selectedPokemonData, setSelectedPokemonData] = useState<pokemonData>({
+    id: 1,
+    name: "bulbasaur",
+    height: 7,
+    weight: 69,
+    male: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    female: undefined,
+    shinyMale: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png",
+    shinyFemale: undefined,
+    abilities: [
+      "chlorophyll",
+      "overgrow"
+      ],
+    types: [
+      "grass",
+      "poison"
+      ]
+  })
+
+  const [formatedPokemonName, setFormatedPokemonName] = useState<string>('')
 
   useEffect(() => {
     setPokemonName(selectedPokemonName)
@@ -29,11 +61,21 @@ const Pokemon: React.FC<pokemon> = ({ selectedPokemonName }) => {
   return (
     <div className="pokemon">
         <span >A</span>
-        <h3 >{ selectedPokemonData.name }</h3>
-        <h3 >{ selectedPokemonData.height }</h3>
-        <h3 >{ selectedPokemonData.weight }</h3>
-        <h3 >{ selectedPokemonData.id }</h3>
+        <h3>{ selectedPokemonData.name }</h3>
+        <h3>{ selectedPokemonData.height }</h3>
+        <h3>{ selectedPokemonData.weight }</h3>
+        <h3>{ selectedPokemonData.id }</h3>
         <img src={ selectedPokemonData.male } ></img>
+        <ol>
+          { selectedPokemonData.abilities.map((ability: string) => 
+            <li key={ability}>{ ability }</li>)
+          }
+        </ol>
+        <ol>
+          { selectedPokemonData.types.map((type: string) => 
+            <li key={type}>{ type }</li>)
+          }
+        </ol>
         <span>A</span>     
     </div>
   )
