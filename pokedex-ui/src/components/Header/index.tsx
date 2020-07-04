aec79ab63c850175cf3ddcacb0982412b7254e5c
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-07-04 00:39:06 -0300

    useEffect is not necessary in this component

--- pokedex-ui/src/components/Header/index.tsx
+++ pokedex-ui/src/components/Header/index.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { FaRegLightbulb } from 'react-icons/fa'
 
 import './styles.css';
 
 const Header: React.FC = () => {
 
-  
+
 
 
 
